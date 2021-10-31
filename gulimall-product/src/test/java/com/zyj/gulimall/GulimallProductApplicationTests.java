e07cba128509e70c5ce1cb60cd93654e0c8c1d06
dr_glb <dr_glb@zyj.com>
2021-10-31 20:15:54 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/zyj/gulimall/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyj.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
