4d46e5beef95a8db47b6104edb938c7dfbf19bd9
Jiang <2412305937@qq.com>
2021-11-21 14:18:13 +0800

    初始化仓库服务&&获取仓库列表及条件查询

--- gulimall-ware/src/main/java/com/zyj/gulimall/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/zyj/gulimall/GulimallWareApplication.java
@@ -1,8 +1,14 @@
 package com.zyj.gulimall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.zyj.gulimall.ware.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
 
--- gulimall-ware/src/main/java/com/zyj/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/zyj/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.zyj.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zyj.common.utils.PageUtils;
 import com.zyj.common.utils.Query;
-
 import com.zyj.gulimall.ware.dao.WareInfoDao;
 import com.zyj.gulimall.ware.entity.WareInfoEntity;
 import com.zyj.gulimall.ware.service.WareInfoService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,21 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and(w -> {
+               w.eq("id", key).or().like("name", key)
+                       .or().like("address", key)
+                       .or().like("areacode", key);
+            });
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
